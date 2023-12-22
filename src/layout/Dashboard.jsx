52110e926fcfaaf3c59ab9218213e162f0799f4f
brcshakil1 <brcshakil72@gmail.com>
2023-12-22 09:15:22 +0600

    get all user task

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,9 +1,17 @@
 import { NavLink, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import { FaPlus } from "react-icons/fa6";
+import { FaHouseMedical, FaPlus } from "react-icons/fa6";
+import { SiTask } from "react-icons/si";
+import { RiMenu2Line } from "react-icons/ri";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const { user, loading } = useAuth();
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -11,33 +19,67 @@ const Dashboard = () => {
   return (
     <div>
       {user?.email ? (
-        <div className="w-full flex min-h-screen">
-          <div className="w-2/12 px-5 py-14 ">
-            <div>
-              <img
-                src={user?.photoURL}
-                className="rounded-full border-4"
-                alt={user?.displayName}
-              />
-              <h3 className="text-xl font-bold">{user?.displayName}</h3>
-              <p>{user?.email}</p>
-            </div>
-            <ul className="pt-2">
-              <NavLink to="create-tasks" className=" ">
-                <li className="flex justify-center items-center gap-2 border py-2  ">
-                  <FaPlus /> <span>Add new tasks</span>
-                </li>
-              </NavLink>
-              <NavLink to="previous-tasks" className="">
-                <li className="">See previous tasks</li>
-              </NavLink>
-              <div className="w-full bg-gray-400 h-[1px] my-3"></div>
-              <NavLink to="/" className="">
-                <li className="">Home</li>
-              </NavLink>
+        <div className="flex flex-col lg:flex-row ">
+          <div
+            className="w-full lg:w-3/12 h-auto  
+          lg:min-h-screen  p-5
+             md:py-10 border-b-2 md:border-r-2 border-gray-500"
+          >
+            <RiMenu2Line
+              className="lg:hidden block  cursor-pointer font-bold text-xl"
+              onClick={handleMenu}
+            />
+
+            <ul
+              className={`absolute ${isOpen ? "block" : "hidden"}
+            lg:block
+          lg:static z-20 bg-gray-200  lg:bg-[#00000000] 
+              md:p-0 p-4 space-y-2 md:space-y-4 rounded-md md:shadow-none shadow-lg`}
+            >
+              <div className="w-full flex ">
+                <div className=" px-5 py-6 md:py-14 ">
+                  <div className="flex justify-center items-center flex-col">
+                    <img
+                      src={user?.photoURL}
+                      className="rounded-full border-b-4 border-r-0 md:border-b-0 md:border-r-4 h-14 w-14 md:w-24 md:h-24"
+                      alt={user?.displayName}
+                    />
+                    <h3 className="text-xl font-bold">{user?.displayName}</h3>
+                    <p>{user?.email}</p>
+                  </div>
+                  <ul className="pt-2">
+                    <NavLink to="create-tasks" className=" ">
+                      <li
+                        className="flex justify-center items-center gap-2 border
+                       border-slate-400 px-3 py-2  "
+                      >
+                        <FaPlus /> <span>Add new tasks</span>
+                      </li>
+                    </NavLink>
+                    <NavLink to="previous-tasks" className=" ">
+                      <li
+                        className="flex justify-center items-center gap-2 mt-2
+                       border-slate-400 border px-3 py-2  "
+                      >
+                        <SiTask /> <span>Your Previous Tasks</span>
+                      </li>
+                    </NavLink>
+                    <NavLink to="" className="">
+                      <li className=""></li>
+                    </NavLink>
+                    <div className="w-full bg-gray-400 h-[1px] my-3"></div>
+                    <NavLink to="/" className="">
+                      <li className="flex justify-center items-center border-slate-400 gap-2 border px-3 py-2 ">
+                        <FaHouseMedical />
+                        Home
+                      </li>
+                    </NavLink>
+                  </ul>
+                </div>
+              </div>
             </ul>
           </div>
-          <div className="w-9/12 py-20 px-5">
+          <div className="w-full md:w-9/12 py-20 px-5">
             <Outlet />
           </div>
         </div>
--- src/pages/Dashboard/PreviousTasks/PreviousTasks.jsx
+++ src/pages/Dashboard/PreviousTasks/PreviousTasks.jsx
@@ -1,7 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import useAuth from "../../../hooks/useAuth";
+import SectionTitle from "../../../components/ui/SectionTitle";
+
 const PreviousTasks = () => {
+  const { user } = useAuth();
+
+  const { data: tasks } = useQuery({
+    queryKey: ["all-matched-tasks"],
+    queryFn: async () => {
+      const res = await axios.get(
+        `http://localhost:3000/tasks?email=${user?.email}`
+      );
+      return res.data;
+    },
+  });
+
   return (
     <div>
-      <h2>Previous Task</h2>
+      <SectionTitle title="Previous Task" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {tasks &&
+          tasks?.map((task) => (
+            <div key={task._id} className="card w-full bg-base-300 shadow-xl">
+              <div className="card-body ">
+                <h2 className="card-title">{task?.title}</h2>
+                <p>{task?.description}</p>
+                <p>Priority: {task?.priority}</p>
+                <p>Deadline: {task?.date}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Edit</button>
+                  <button className="btn btn-ghost text-red-400">Delete</button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
